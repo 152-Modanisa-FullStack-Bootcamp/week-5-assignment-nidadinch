aa0968be416373dfff0981d567bce50f378b0601
Nida Dinç <20160808047@ogr.akdeniz.edu.tr>
2022-02-07 00:13:14 +0300

    add entire tests & implementation

--- assignment/assignment.go
+++ assignment/assignment.go
@@ -1,6 +1,10 @@
 package assignment
 
-import "math"
+import (
+	"math"
+	"reflect"
+	"strings"
+)
 
 func AddUint32(x, y uint32) (uint32, bool) {
 	z := x + y
@@ -11,21 +15,89 @@ func AddUint32(x, y uint32) (uint32, bool) {
 }
 
 func CeilNumber(f float64) float64 {
-	return 0
+	x := math.Ceil(f*4) / 4
+	return x
 }
 
 func AlphabetSoup(s string) string {
-	return ""
+	var resultStr string
+	strRune := []rune(s)
+	// bubble sort
+	for i := 0; i < len(strRune); i++ {
+		for j := 1; j < len(strRune)-i; j++ {
+			if strRune[j] < strRune[j-1] {
+				strRune[j], strRune[j-1] = strRune[j-1], strRune[j]
+			}
+		}
+	}
+	for _, v := range strRune {
+		resultStr += string(v)
+	}
+	return resultStr
 }
 
 func StringMask(s string, n uint) string {
-	return ""
+	var resultStr string
+	str1 := s[:n]
+	str2 := s[n:]
+
+	if len(s) == 0 {
+		return "*"
+	}
+
+	if int(n) == len(s) || int(n) > len(s) {
+		for i := 0; i < len(s); i++ {
+			resultStr += "*"
+		}
+		return resultStr
+	}
+
+	for i := 0; i < len(str2); i++ {
+		resultStr += "*"
+	}
+
+	return str1 + resultStr
 }
 
 func WordSplit(arr [2]string) string {
-	return ""
+	firstStr := arr[0]
+	words := strings.Split(arr[1], ",")
+	resultStr := ""
+
+	for _, s := range words {
+		if index := strings.Index(firstStr, s); index != -1 {
+			splitted1 := firstStr[:index]
+			splitted2 := firstStr[index:]
+
+			if strings.Contains(arr[1], splitted1) {
+				resultStr = splitted1 + "," + splitted2
+				return resultStr
+			}
+		}
+	}
+	return "not possible"
 }
 
+// Implementation for Final Boss using Reflect
+func VariadicSet2(i ...interface{}) []interface{} {
+	count := 0
+	s := reflect.ValueOf(i)
+	result := make([]interface{}, s.Len())
+
+	for j := 0; j < s.Len(); j++ {
+		if !Contains(result, s.Index(j).Interface()) {
+			result[j] = s.Index(j).Interface()
+			count++
+		}
+	}
+	// I have created another slice to get rid of nil values
+	// Because 'result' contains nil values
+	finalRes := make([]interface{}, count)
+	copy(finalRes, result)
+	return finalRes
+}
+
+// Implementation for Final Boss using traditional method
 func VariadicSet(i ...interface{}) []interface{} {
 	return nil
 }
--- assignment/assignment_test.go
+++ assignment/assignment_test.go
@@ -85,3 +85,155 @@ func TestCeilNumber(t *testing.T) {
 		})
 	}
 }
+
+func TestAlphabetSoup(t *testing.T) {
+
+	tests := []struct {
+		name string
+		s    string
+		want string
+	}{
+		/*
+			String with the letters in alphabetical order.
+			cases need to pass:
+			 	"hello" => "ehllo"
+				"" => ""
+				"h" => "h"
+				"ab" => "ab"
+				"ba" => "ab"
+				"bac" => "abc"
+				"cba" => "abc"
+		*/
+		{"testCase01", "hello", "ehllo"},
+		{"testCase02", "", ""},
+		{"testCase03", "h", "h"},
+		{"testCase04", "ab", "ab"},
+		{"testCase05", "ba", "ab"},
+		{"testCase06", "bac", "abc"},
+		{"testCase07", "cba", "abc"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := AlphabetSoup(tt.s)
+			assert.Equal(t, tt.want, result)
+		})
+	}
+}
+
+func TestStringMask(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		str   string
+		count int
+		want  string
+	}{
+		/*
+			Replace after n(uint) character of string with '*' character.
+			cases need to pass:
+				"!mysecret*", 2 => "!m********"
+				"", n(any positive number) => "*"
+				"a", 1 => "*"
+				"string", 0 => "******"
+				"string", 3 => "str***"
+				"string", 5 => "strin*"
+				"string", 6 => "******"
+				"string", 7(bigger than len of "string") => "******"
+				"s*r*n*", 3 => "s*r***"
+		*/
+		{"testCase01", "!mysecret*", 2, "!m********"},
+		{"testCase02", "", 1, "*"},
+		{"testCase03", "a", 1, "*"},
+		{"testCase04", "string", 0, "******"},
+		{"testCase05", "string", 3, "str***"},
+		{"testCase06", "string", 5, "strin*"},
+		{"testCase07", "string", 7, "******"},
+		{"testCase08", "s*r*n*", 3, "s*r***"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := StringMask(tt.str, uint(tt.count))
+			assert.Equal(t, tt.want, result)
+		})
+	}
+}
+
+func TestWordSplit(t *testing.T) {
+	words := "apple,bat,cat,goodbye,hello,yellow,why"
+
+	tests := []struct {
+		name     string
+		givenStr string
+		wantStr  string
+	}{
+		/*
+			Your goal is to determine if the first element in the array can be split into two words,
+			where both words exist in the dictionary(words variable) that is provided in the second element of array.
+			cases need to pass:
+				[2]string{"hellocat",words} => hello,cat
+				[2]string{"catbat",words} => cat,bat
+				[2]string{"yellowapple",words} => yellow,apple
+				[2]string{"",words} => not possible
+				[2]string{"notcat",words} => not possible
+				[2]string{"bootcamprocks!",words} => not possible
+		*/
+		{"testCase01", "hellocat", "hello,cat"},
+		{"testCase02", "catbat", "cat,bat"},
+		{"testCase03", "yellowapple", "yellow,apple"},
+		{"testCase04", "", "not possible"},
+		{"testCase05", "notcat", "not possible"},
+		{"testCase06", "bootcamprocks!", "not possible"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := WordSplit([2]string{tt.givenStr, words})
+			assert.Equal(t, tt.wantStr, result)
+		})
+	}
+}
+
+func TestVariadicSet(t *testing.T) {
+
+	tests := []struct {
+		name string
+		list []interface{}
+		want []interface{}
+	}{
+		/*
+			FINAL BOSS ALERT :)
+			Tip: Learn and apply golang variadic functions(search engine -> "golang variadic function" -> WOW You can really dance! )
+			Convert inputs to set(no duplicate element)
+			cases need to pass:
+				4,2,5,4,2,4 => []interface{4,2,5}
+				"bootcamp","rocks!","really","rocks!"=> []interface{"bootcamp","rocks!","really"}
+				1,uint32(1),"first",2,uint32(2),"second",1,uint32(2),"first" => []interface{1,uint32(1),"first",2,uint32(2),"second"}
+		*/
+		{"TestCase01", []interface{}{4, 2, 5, 4, 2, 4}, []interface{}{4, 2, 5}},
+		{"TestCase02", []interface{}{"bootcamp", "rocks!", "really", "rocks!"}, []interface{}{"bootcamp", "rocks!", "really"}},
+		{"TestCase03", []interface{}{1, uint32(1), "first", 2, uint32(2), "second", 1, uint32(2), "first"}, []interface{}{1, uint32(1), "first", 2, uint32(2), "second"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			set := VariadicSet(tt.list...)
+			assert.Equal(t, tt.want, set)
+		})
+		t.Run(tt.name, func(t *testing.T) {
+			set := VariadicSet2(tt.list...)
+			assert.Equal(t, tt.want, set)
+		})
+	}
+}
+
+//BenchmarkVariadicSet-4           3125035               393.6 ns/op           240 B/op          4 allocs/op
+func BenchmarkVariadicSet(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		VariadicSet(1, uint32(1), "first", 2, uint32(2), "second", 1, uint32(2), "first")
+	}
+}
+
+//BenchmarkVariadicSet2-4          1884538               576.6 ns/op           408 B/op          4 allocs/opfunc BenchmarkVariadicSet2(b *testing.B) {
+func BenchmarkVariadicSet2(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		VariadicSet2(1, uint32(1), "first", 2, uint32(2), "second", 1, uint32(2), "first")
+	}
+}
